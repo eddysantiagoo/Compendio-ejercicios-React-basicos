6a933e9aa1e66d1cc40d88db59b54428bc5e179b
eddysantiagoo <eddysantiagogh@gmail.com>
2022-08-18 12:25:30 -0500

    Inicio segundo ejercicio, creacion componente formulario

---
+++ 02-widget-clima/src/components/WeatherApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function WeatherApp() {
+  const [weather, setWeather] = useState(null);
+
+  return (
+    <div>
+      <form></form>
+      <div></div>
+    </div>
+  );
+}
